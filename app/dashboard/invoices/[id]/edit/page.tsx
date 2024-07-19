b3d23667f47bbeeb4823f8ca407467f27d78f477
zenggenghai <zenggenghai>
2024-07-19 18:26:19 +0800

    feat: config eslint prettier husky.
    Signed-off-by: zenggenghai <zenggenghai>

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -6,7 +6,7 @@ import { notFound } from "next/navigation";
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
+    fetchInvoiceById(id).catch(() => {}),
     fetchCustomers(),
   ]);
   if (!invoice) {

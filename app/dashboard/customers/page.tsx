85d95f7f7a5932c4819ac69134aec25f8b5a2837
zenggenghai <zenggenghai>
2024-07-23 11:43:46 +0800

    feat: seo.
    Signed-off-by: zenggenghai <zenggenghai>

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "客户页面",
+};
 export default function Page() {
   return <p>Customers Page</p>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | 曾耿海",
+    default: "曾耿海 nextjs 网站练习",
+  },
+  description: "根据官网项目教程学习 nextjs",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
